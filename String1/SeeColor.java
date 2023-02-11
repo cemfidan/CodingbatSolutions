ba311983d030051d5224a050c3c7ee5f5eead145
Cem Fidan <112818050+cemfidan@users.noreply.github.com>
2023-02-11 23:38:06 +0300

    Add files via upload

---
+++ String1/SeeColor.java
@@ -0,0 +1,24 @@
+public class SeeColor {
+
+	/*
+	 * 
+	 * Given a string, if the string begins with "red" or "blue" return that color
+	 * string, otherwise return the empty string.
+	 * 
+	 * 
+	 * seeColor("redxx") → "red" 
+	 * seeColor("xxred") → "" 
+	 * seeColor("blueTimes") → "blue"
+	 */
+
+	public String seeColor(String str) {
+		if (str.length() > 2 && str.substring(0, 3).equals("red")) {
+			return "red";
+		}
+		if (str.length() > 3 && str.substring(0, 4).equals("blue")) {
+			return "blue";
+		}
+		return "";
+	}
+
+}
